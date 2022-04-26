866b88bc0e0c9b39e0e5668e06c1716845267fc0
Thanh Nguyen <thnguyen1847@gmail.com>
2022-04-26 15:16:48 +0900

    created a delete trip view

--- mygrouptravelsite/home/urls.py
+++ mygrouptravelsite/home/urls.py
@@ -6,6 +6,8 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.HomePage, name='home_page' ),
-    path('trips/', views.TripListView.as_view(), name='trip_list' ),
-    path('trip_create/', views.TripCreateView.as_view() , name='trip_create')
+    path('trips/', views.TripListView.as_view(), name='trip_list'),
+    path('trip_create/', views.TripCreateView.as_view() , name='trip_create'),
+    path('trips/<int:pk>', views.TripDetailView.as_view(), name='trip_detail'),
+    path('trip/<int:pk>/delete', views.TripDeleteView.as_view(), name='trip_delete'),
 ]
--- mygrouptravelsite/home/views.py
+++ mygrouptravelsite/home/views.py
@@ -7,15 +7,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Trip
 
-from mygrouptravelsite.owner import OwnerListView
+from mygrouptravelsite.owner import OwnerListView, OwnerDetailView, OwnerDeleteView
 from home.forms import TripForm
 
 
 # Create your views here.
 
 def HomePage(request):
-    html = "<html><body>Welcome to my front page!</body></html>"
-    return HttpResponse(html)
+    template_name = "home/home_page.html"
+    return render (request, template_name)
 
 
 class TripListView(OwnerListView):
@@ -25,10 +25,8 @@ class TripListView(OwnerListView):
 
 class TripCreateView(LoginRequiredMixin, View):
     model = Trip
-    
     template_name = 'home/trip_form.html'
     fields = ['name', 'start_date', 'end_date']
-    
     success_url = reverse_lazy('home:trip_list')
 
     def get(self, request, pk=None):
@@ -38,7 +36,6 @@ class TripCreateView(LoginRequiredMixin, View):
     
     def post(self, request, pk=None):
         form = TripForm(request.POST)
-
         if not form.is_valid():
             ctx = {'form':form}
             return render(request, self.template_name, ctx)
@@ -46,6 +43,22 @@ class TripCreateView(LoginRequiredMixin, View):
         trip.owner = self.request.user
         trip.save()
         return redirect(self.success_url)
+
+class TripDetailView(OwnerDetailView, View):
+    model = Trip
+    template_name = "home/trip_detail.html"
+
+    def get(self, request, pk):
+        trip = Trip.objects.get(id=pk)
+        ctx = {'trip': trip}
+        return render(request, self.template_name, ctx)
+
+class TripDeleteView(OwnerDeleteView):
+    model = Trip
+    success_url = reverse_lazy('home:trip_list')
+
+
+
 
 
     
