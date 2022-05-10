f822fef3fe77277d08dae0b2d377bf5bf0388a65
colormethanh <thnguyen1847@gmail.com>
2022-05-10 09:37:54 -0400

    commit

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\takan\OneDrive\デスクトップ\Thanhs_projects\GroupTravelOrganizerWebsite\.venv
+set VIRTUAL_ENV=C:\Users\thanh\OneDrive\Desktop\my_projects\GroupTravelOrganizerWebsite\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
